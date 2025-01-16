a1092f7e6d81c0ad8dcb33e4e4982f32085e8136
Sameer Gupta <121657688+Guptasameer533@users.noreply.github.com>
2025-01-16 19:54:26 +0530

    Update Game.tsx

--- components/Game.tsx
+++ components/Game.tsx
@@ -5,7 +5,7 @@ import RainGrid from './RainGrid';
 const Game: React.FC = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <RainGrid width={20} height={15} isPlaying={true} speed={175} />
+      <RainGrid width={20} height={15} isPlaying={true} speed={185} />
     </div>
   );
 };
